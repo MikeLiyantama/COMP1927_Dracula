d70e12f54b4e65b1792701f58e44051198ab2f62
MikeLiyantama <michael.liyantama@live.com>
2015-10-10 20:31:51 +1100

    Update 1
    Hunter's Dracula hunt & Dracula's random movement.

--- phase2/dracula.c
+++ phase2/dracula.c
@@ -8,7 +8,24 @@
 
 void decideDraculaMove(DracView gameState)
 {
-	// TODO ...
-	// Replace the line below by something better
-	registerBestPlay("CD","Mwuhahahaha");
+	if(giveMeTheRound(gameState) == 1){
+		//Initial Round
+		registerBestPlay("GE","Greetings");
+	}
+	else{
+		//Subsequent Rounds
+		char dest[3]; //Destination var
+		 
+		//Go to random places
+        int size , *reachableLocation;
+        int magicNum = giveMeTheRound(gameState);
+        reachableLocation = whereCanIgo(gameState,&size,1,0,0);
+        while(magicNum > size){
+            magicNum = magicNum % size;
+        }
+        LocationCopy(edges[magicNum],dest);
+        registerBestPlay(dest,"Nothing to find here");
+		free(dest);
+        return;
+	}
 }
